4f1555b6b64c3a39ce33337fa5e3c2f269e3479b
Vinicius Lima <vinylimaz@live.com>
2020-09-02 21:35:46 -0300

    Add option for the surface dialog component

--- app/components/arara/dialog_component.rb
+++ app/components/arara/dialog_component.rb
@@ -3,15 +3,16 @@ module Arara
     validates :content, presence: true
 
     include Arara::BaseComponent
-    
-    attr_reader :role, :labelledby, :describedby
 
-    def initialize(role: "alertdialog", labelledby:, describedby:, **kw)
+    attr_reader :role, :labelledby, :describedby, :surface_class
+
+    def initialize(role: "alertdialog", labelledby:, describedby:, surface_class: nil, **kw)
       super(tag: "div", role: role, **kw)
 
       @role = role
       @labelledby = labelledby
       @describedby = describedby
+      @surface_class = surface_class
     end
 
 
@@ -20,8 +21,8 @@ module Arara
     end
 
     def surface_options
-      opts = {
-        class: "mdc-dialog__surface",
+      {
+        class: "mdc-dialog__surface #{surface_class}",
         role: role,
         aria: {
           modal: "true",
